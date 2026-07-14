2a41b35389a86b42caf3e275c913484dafc0bdc7
TPA170 <tpisarevski170@gmail.com>
2026-07-14 20:43:24 +0300

    connected i hope

--- backEnd/src/auth/dto/login.dto.ts
+++ backEnd/src/auth/dto/login.dto.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'Person' })
+  @ApiProperty({ example: 'user@example.com' })
   @IsString({ message: 'נא להזין אימייל' })
   @MinLength(1, { message: 'נא להזין אימייל' })
   email: string;
 
-  @ApiProperty({ example: 'Person' })
+  @ApiProperty({ example: '••••••••' })
   @IsString({ message: 'נא להזין סיסמה' })
   @MinLength(1, { message: 'נא להזין סיסמה' })
   password: string;
